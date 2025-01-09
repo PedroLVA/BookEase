14b2c0779976726e7beec74310402d3017fdba70
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-09 10:59:16 -0300

    refactor: Remove unused imports in TicketService class.

--- back/src/main/java/com/bookease/bookease/services/TicketService.java
+++ back/src/main/java/com/bookease/bookease/services/TicketService.java
@@ -1,17 +1,11 @@
 package com.bookease.bookease.services;
-
-import com.bookease.bookease.domain.Event;
 import com.bookease.bookease.domain.Ticket;
-import com.bookease.bookease.domain.User;
 import com.bookease.bookease.dtos.mappers.TicketMapper;
 import com.bookease.bookease.dtos.ticket.TicketRequestDTO;
 import com.bookease.bookease.dtos.ticket.TicketResponseDTO;
-import com.bookease.bookease.repositories.EventRepository;
 import com.bookease.bookease.repositories.TicketRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.stream.Collectors;
 
