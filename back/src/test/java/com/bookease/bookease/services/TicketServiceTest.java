65b951fbfb37a1814ae56496aad60a065c1f04bc
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-16 16:14:31 -0300

    feat: Create ticket service test class.

---
+++ back/src/test/java/com/bookease/bookease/services/TicketServiceTest.java
@@ -0,0 +1,35 @@
+package com.bookease.bookease.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketServiceTest {
+
+    @Test
+    @DisplayName("Should create ticket successfully")
+    void createTicketCase1() {
+    }
+
+    @Test
+    @DisplayName("Should not be able to find event to create ticket")
+    void createTicketCase2() {
+    }
+
+    @Test
+    @DisplayName("Should not be able to create ticket because event is full")
+    void createTicketCase3() {
+    }
+
+
+    @Test
+    @DisplayName("Should be able to delete ticket successfully")
+    void deleteTicketCase1() {
+    }
+
+    @Test
+    @DisplayName("Should be able to delete ticket successfully")
+    void deleteTicketCase2() {
+    }
+}
