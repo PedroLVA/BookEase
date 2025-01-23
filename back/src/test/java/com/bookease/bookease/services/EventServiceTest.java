25c161b9b2d8796ea11838e49c51495660618588
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-23 15:52:37 -0300

    feat: add create event test.

--- back/src/test/java/com/bookease/bookease/services/EventServiceTest.java
+++ back/src/test/java/com/bookease/bookease/services/EventServiceTest.java
@@ -1,17 +1,24 @@
 package com.bookease.bookease.services;
 
+import com.bookease.bookease.domain.Event;
 import com.bookease.bookease.domain.Organizer;
 import com.bookease.bookease.domain.Role;
 import com.bookease.bookease.dtos.event.EventRequestDTO;
 import com.bookease.bookease.dtos.mappers.EventMapper;
 import com.bookease.bookease.repositories.EventRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDateTime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class EventServiceTest {
 
@@ -73,7 +80,34 @@ class EventServiceTest {
     }
 
     @Test
+    @DisplayName("Should create event successfully and return the right event")
     void createEvent() {
+        Event mockEvent = new Event();
+        mockEvent.setId("e1");
+        mockEvent.setName("Mock");
+        mockEvent.setDescription("Mock desc");
+        mockEvent.setStartingDate(LocalDateTime.parse("2024-09-05T16:00:00"));
+        mockEvent.setEndingDate(LocalDateTime.parse("2024-10-05T16:00:00"));
+        mockEvent.setCapacity(100);
+        mockEvent.setAddress("101 Code Street");
+        mockEvent.setCity("Sorocaba");
+        mockEvent.setState("SP");
+        mockEvent.setOrganizer(mockOrganizer);
+
+        Mockito.when(eventMapper.toEntity(mockEventRequestDTO, mockOrganizer)).thenReturn(mockEvent);
+
+        Mockito.when(eventRepository.save(mockEvent)).thenReturn(mockEvent);
+
+        Event result = eventService.createEvent(mockEventRequestDTO, mockOrganizer);
+
+        assertNotNull(result);
+        assertEquals(result.getId(), mockEvent.getId());
+        assertEquals(mockEvent.getName(), result.getName());
+        assertEquals(mockEvent.getDescription(), result.getDescription());
+        assertEquals(mockEvent.getOrganizer(), result.getOrganizer());
+
+        Mockito.verify(eventMapper, Mockito.times(1)).toEntity(mockEventRequestDTO, mockOrganizer);
+        Mockito.verify(eventRepository, Mockito.times(1)).save(mockEvent);
 
     }
 
