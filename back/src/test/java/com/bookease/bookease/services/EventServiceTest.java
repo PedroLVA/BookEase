9f361de53b2407e231c8c1555e18956ea2268c28
Pedro Athayde <pedro_athayde@outlook.com>
2025-01-23 15:37:40 -0300

    feat: add test setup config.

--- back/src/test/java/com/bookease/bookease/services/EventServiceTest.java
+++ back/src/test/java/com/bookease/bookease/services/EventServiceTest.java
@@ -1,11 +1,17 @@
 package com.bookease.bookease.services;
 
+import com.bookease.bookease.domain.Organizer;
+import com.bookease.bookease.domain.Role;
+import com.bookease.bookease.dtos.event.EventRequestDTO;
 import com.bookease.bookease.dtos.mappers.EventMapper;
 import com.bookease.bookease.repositories.EventRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
 
 class EventServiceTest {
 
@@ -19,9 +25,43 @@ class EventServiceTest {
     @InjectMocks
     private EventService eventService;
 
+    private EventRequestDTO mockEventRequestDTO;
+
+    private Organizer mockOrganizer;
+
     @BeforeEach
     void setup(){
+        MockitoAnnotations.openMocks(this);
         //Setup
+        //event request DTO
+
+        LocalDateTime startDate = LocalDateTime.parse("2024-09-05T16:00:00");
+        LocalDateTime endDate = LocalDateTime.parse("2024-10-05T16:00:00");
+
+        EventRequestDTO eventRequestDTO = new EventRequestDTO("Mock",
+                "Mock desc",
+                startDate,
+                endDate,
+                100,
+                "101 Code Street",
+                "Sorocaba",
+                "SP",
+                "10");
+
+        mockEventRequestDTO = eventRequestDTO;
+
+        Organizer organizer = new Organizer();
+        //config inicial
+        organizer.setId("53736b4c-c5cf-4d17-bb5b-e1fa70a3010c");
+        organizer.setName("organizer");
+        organizer.setEmail("organizer.ig@example.com");
+        organizer.setPassword("Rockasa");
+        organizer.setPhoneNumber("+1234567890");
+        organizer.setDateOfBirth(LocalDateTime.now());
+        organizer.setRole(Role.ORGANIZER);
+
+        mockOrganizer = organizer;
+
     }
 
     @Test
@@ -34,6 +74,7 @@ class EventServiceTest {
 
     @Test
     void createEvent() {
+
     }
 
     @Test
