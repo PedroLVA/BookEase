817f15ea2ca79a6983c133aa1428de329e881771
Pedro Athayde <pedro_athayde@outlook.com>
2025-04-04 16:28:48 -0300

    feat: add CategoryServiceIntegrationTest and add method to CategoryRepository.

--- back/src/main/java/com/bookease/bookease/repositories/CategoryRepository.java
+++ back/src/main/java/com/bookease/bookease/repositories/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.bookease.bookease.repositories;
 import com.bookease.bookease.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 
 public interface CategoryRepository extends JpaRepository<Category, String> {
+    public Optional<Category> findByName(String name);
 }
---
+++ back/src/test/java/com/bookease/bookease/integration/CategoryServiceIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.bookease.bookease.integration;
+
+import com.bookease.bookease.domain.Category;
+import com.bookease.bookease.dtos.category.CategoryRequestDTO;
+import com.bookease.bookease.repositories.CategoryRepository;
+import com.bookease.bookease.repositories.EventRepository;
+import com.bookease.bookease.repositories.OrganizerRepository;
+import com.bookease.bookease.repositories.TicketRepository;
+import com.bookease.bookease.services.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class CategoryServiceIntegrationTest {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Autowired
+    private OrganizerRepository organizerRepository;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @BeforeEach
+    public void setup(){
+        ticketRepository.deleteAll();
+        eventRepository.deleteAll();
+        organizerRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+    }
+
+    private CategoryRequestDTO createCategoryRequest(String name) {
+        return new CategoryRequestDTO("TestId", name);
+    }
+
+    @Test
+    public void createCategory_ShouldSaveCategoryToDatabase(){
+        CategoryRequestDTO requestDTO = createCategoryRequest("Fantasy");
+        long initialCount = categoryRepository.count();
+
+        // Act
+        ResponseEntity response = categoryService.createCategory(requestDTO);
+
+        // Assert
+        assertEquals(initialCount + 1, categoryRepository.count());
+        assertEquals(200, response.getStatusCode().value());
+
+        Optional<Category> savedCategory = categoryRepository.findByName("Fantasy");
+        assertTrue(savedCategory.isPresent());
+        assertEquals("Fantasy", savedCategory.get().getName());
+    }
+
+
+}
